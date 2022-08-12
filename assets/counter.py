8a7e11e65e208c6314f755f7dbc5d9c1b281a532
in45 <i.hasnaoui@shellboxes.com>
2022-08-12 23:44:48 +0000

    fix prog

--- assets/counter.py
+++ assets/counter.py
@@ -18,12 +18,8 @@ def approval():
             ]
         )
 
-    handle_optin = Seq([
-        Assert(App.optedIn(Txn.sender(), Txn.application_id())),
-        Return(Int(1))
-    ])
 
-   increment = Seq(
+    increment = Seq(
         [
             App.globalPut(global_counter, App.globalGet(global_counter) + Int(1)),
             Approve(),
@@ -40,21 +36,14 @@ def approval():
     handle_noop = Cond(
             [Txn.application_args[0] == op_increment, increment],
             [Txn.application_args[0] == op_decrement, decrement],
-        )
-    handle_closeout = Return(Int(1))
-    handle_updateapp = Return(Int(0))
-    handle_deleteapp = Return(Int(0))
-
+    )
+  
     program = Cond(
         [Txn.application_id() == Int(0), handle_creation],
-        [Txn.on_completion() == OnComplete.OptIn, handle_optin],
-        [Txn.on_completion() == OnComplete.CloseOut, handle_closeout],
-        [Txn.on_completion() == OnComplete.UpdateApplication, handle_updateapp],
-        [Txn.on_completion() == OnComplete.DeleteApplication, handle_deleteapp],
         [Txn.on_completion() == OnComplete.NoOp, handle_noop]
     )
 
     return program
 
 if __name__ == "__main__":
-    print(compileTeal(approval(), mode=Mode.Application, version=6))
+    print(compileTeal(approval(), mode=Mode.Application, version=5))

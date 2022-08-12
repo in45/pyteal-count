2a7bcc46f207853e6e12739f613a33c97c6a92cd
Inas Hasnaoui <i.hasnaoui@shellboxes.com>
2022-08-13 00:33:45 +0100

    update program

--- assets/counter.py
+++ assets/counter.py
@@ -1,17 +1,29 @@
 from pyteal import *
-from pyteal.ast.bytes import Bytes
-from helpers import program
-
 
 def approval():
-    # globals
+
     global_owner = Bytes("owner")  # byteslice
     global_counter = Bytes("counter")  # uint64
 
     op_increment = Bytes("inc")
     op_decrement = Bytes("dec")
+    '''
+    Initialization
+    '''
+    handle_creation = Seq(
+            [
+                App.globalPut(global_owner, Txn.sender()),
+                App.globalPut(global_counter, Int(0)),
+                Approve(),
+            ]
+        )
 
-    increment = Seq(
+    handle_optin = Seq([
+        Assert(App.optedIn(Txn.sender(), Txn.application_id())),
+        Return(Int(1))
+    ])
+
+   increment = Seq(
         [
             App.globalPut(global_counter, App.globalGet(global_counter) + Int(1)),
             Approve(),
@@ -25,20 +37,24 @@ def approval():
         ]
     )
 
-    return program.event(
-        init=Seq(
-            [
-                App.globalPut(global_owner, Txn.sender()),
-                App.globalPut(global_counter, Int(0)),
-                Approve(),
-            ]
-        ),
-        no_op=Cond(
+    handle_noop = Cond(
             [Txn.application_args[0] == op_increment, increment],
             [Txn.application_args[0] == op_decrement, decrement],
-        ),
+        )
+    handle_closeout = Return(Int(1))
+    handle_updateapp = Return(Int(0))
+    handle_deleteapp = Return(Int(0))
+
+    program = Cond(
+        [Txn.application_id() == Int(0), handle_creation],
+        [Txn.on_completion() == OnComplete.OptIn, handle_optin],
+        [Txn.on_completion() == OnComplete.CloseOut, handle_closeout],
+        [Txn.on_completion() == OnComplete.UpdateApplication, handle_updateapp],
+        [Txn.on_completion() == OnComplete.DeleteApplication, handle_deleteapp],
+        [Txn.on_completion() == OnComplete.NoOp, handle_noop]
     )
 
+    return program
 
-def clear():
-    return Approve()
+if __name__ == "__main__":
+    print(compileTeal(approval(), mode=Mode.Application, version=6))

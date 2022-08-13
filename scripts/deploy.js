a661c965e7e9a3efa4edab44837ddb4645d14066
Inas Hasnaoui <i.hasnaoui@shellboxes.com>
2022-08-13 01:01:52 +0100

    add deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,40 +1,43 @@
-const {
-  executeTransaction
-} = require('@algo-builder/algob');
-const { types } = require('@algo-builder/runtime');
+const { executeTransaction, convert } = require("@algo-builder/algob");
+const { types } = require("@algo-builder/web");
 
-async function run (runtimeEnv, deployer) {
-  const masterAccount = deployer.accountsByName.get('master-account');
-  const creatorAccount = deployer.accountsByName.get('alice');
+async function run(runtimeEnv, deployer) {
+    const master = deployer.accountsByName.get("master");
+    const approvalFile = "counter_approval.py";
+    const clearStateFile = "counter_clearstate.py";
 
-  const algoTxnParams = {
-    type: types.TransactionType.TransferAlgo,
-    sign: types.SignType.SecretKey,
-    fromAccount: masterAccount,
-    toAccountAddr: creatorAccount.addr,
-    amountMicroAlgos: 200e6,
-    payFlags: {}
-  };
-  // transfer some algos to creator account
-  await executeTransaction(deployer, algoTxnParams);
+    await deployer.deployApp(
+        approvalFile,
+        clearStateFile,
+        {
+            sender: master,
+            localInts: 0,
+            localBytes: 0,
+            globalInts: 1,
+            globalBytes: 1,
+            appArgs: [],
+        },
+        { totalFee: 1000 }
+    );
 
-  // Create Application
-  // Note: An Account can have maximum of 10 Applications.
-  const sscInfo = await deployer.deploySSC(
-    'approval_program.teal', // approval program
-    'clear_program.teal', // clear program
-    {
-      sender: creatorAccount,
-      localInts: 1,
-      localBytes: 1,
-      globalInts: 1,
-      globalBytes: 1
-    }, {});
+    // get app info
+    const counterApp = deployer.getApp(approvalFile, clearStateFile);
+    const counterAppAddress = counterApp.applicationAccount;
+    console.log("app account address:", counterAppAddress);
 
-  console.log(sscInfo);
+    // fund account with 20 algos
+    await executeTransaction(deployer, {
+        type: types.TransactionType.TransferAlgo,
+        sign: types.SignType.SecretKey,
+        fromAccount: master,
+        toAccountAddr: counterAppAddress,
+        amountMicroAlgos: 2e7, //20 algos
+        payFlags: { totalFee: 1000 },
+    });
 
-  // Opt-In for creator
-  await deployer.optInAccountToSSC(creatorAccount, sscInfo.appID, {}, {});
+    // get app account balance
+    let appAccount = await deployer.algodClient.accountInformation(counterAppAddress).do();
+    console.log(appAccount);
 }
 
 module.exports = { default: run };
